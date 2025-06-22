b6c134e5085e56b24a84035f1a2b05fac4a672ee
Luachin <human.connect.3456@gmail.com>
2025-06-22 11:57:52 +0700

    lay du lieu Dat khach

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
+const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRMh8AAHR9OI6EjTRPAL-r4vmjnjIF2qcvT1sv1eZeXZsCTFSgaRmasmVk1WYSxFaiJE3bNW6I5noqZ/pub?gid=891794971&single=true&output=csv';
+const { parse } = require('csv-parse/sync');
+
 app.use(cors());
 
 app.use(express.json());
@@ -13,6 +16,27 @@ app.post('/sms', (req, res) => {
     res.send({ status: 'OK' });
 });
 
+// Route GET để fetch dữ liệu từ Google Sheets
+app.get('/data-from-sheet', async (req, res) => {
+  try {
+    const response = await fetch(csvUrl);
+    const csvText = await response.text();
+
+    
+    const records = parse(csvText, {
+      columns: true,             // Tự lấy dòng đầu làm tên trường
+      skip_empty_lines: true,
+      trim: true
+    });
+
+    res.json(records); 
+  } catch (err) {
+    console.error('Lỗi khi fetch dữ liệu:', err);
+    res.status(500).send('Không lấy được dữ liệu từ Google Sheet');
+  }
+});
+
+
 app.listen(3000, '0.0.0.0', () => {
-  console.log(`📡 Server đang chạy tại http://192.168.1.100:3000`);
+  console.log(`📡 Server đang chạy tại http://100.119.104.80:3000/`);
 });
