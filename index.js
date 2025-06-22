c1a9a40d065d09c159233869e71e79852d9a20a7
Luachin <human.connect.3456@gmail.com>
2025-06-22 09:35:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post('/sms', (req, res) => {
+    const data = req.body;
+    console.log('Tin nhắn nhận được:', data);
+    res.send({ status: 'OK' });
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`📡 Server đang chạy tại http://192.168.1.100:3000`);
+});
